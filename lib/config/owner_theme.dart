da8d6a4baa4c96b54211f50b1e77970765982582
nyoentea <abyandika031@gmail.com>
2024-10-23 20:06:28 +0700

    theme kedua

---
+++ lib/config/owner_theme.dart
@@ -0,0 +1,93 @@
+import 'package:flutter/material.dart';
+
+Color whitecolor = const Color(0xFFFFFFFF);
+
+TextStyle titleTextStyle = TextStyle (
+  fontFamily: 'Poppins',
+  color : whitecolor,
+  fontWeight: FontWeight.bold,
+  fontSize: 26,
+);
+
+TextStyle titleProTextStyle = const TextStyle (
+  fontFamily: 'Poppins',
+  color: Color(0xff007dff),
+  fontWeight: FontWeight.bold,
+  fontSize: 26,
+);
+
+TextStyle subTitleTextStyle = const TextStyle (
+  fontFamily: 'Poppins',
+  color: Color(0xff899788),
+  fontWeight: FontWeight.w300,
+  fontSize: 16,
+);
+
+TextStyle plainTextStyle = TextStyle (
+  fontFamily: 'Poppins',
+  color: whitecolor,
+  fontWeight: FontWeight.w700,
+  fontSize: 18,
+);
+
+TextStyle decTextStyle = TextStyle (
+  fontFamily: 'Poppins',
+  color: whitecolor,
+  fontWeight: FontWeight.w300,
+  fontSize: 12,
+); 
+
+TextStyle priceTextStyle = TextStyle (
+  fontFamily: 'Poppins',
+  color: whitecolor,
+  fontWeight: FontWeight.w700,
+  fontSize: 18,
+);
+
+TextStyle buttonTextStyle = TextStyle (
+  fontFamily: 'Poppins',
+  color: whitecolor,
+  fontWeight: FontWeight.w700,
+  fontSize: 16,
+);
+
+TextStyle buttonStyle =  const TextStyle (
+  fontFamily: 'Poppins',
+  color: Color(0xffffffff),
+  fontWeight: FontWeight.w700,
+  fontSize: 16,
+);
+
+TextStyle notifikasiStyle =  const TextStyle (
+  fontFamily: 'Poppins',
+  color: Color(0xffffffff),
+  fontWeight: FontWeight.w500,
+  fontSize: 20,
+);
+
+TextStyle lampiranStyle = const TextStyle (
+  fontFamily: 'Poppins',
+  color: Colors.black,
+  fontWeight: FontWeight.w600,
+  fontSize: 14,
+);
+
+TextStyle dec2TextStyle = const TextStyle (
+  fontFamily: 'Poppins',
+  color: Colors.black,
+  fontWeight: FontWeight.w400,
+  fontSize: 11,
+); 
+TextStyle dec3TextStyle = const TextStyle (
+  fontFamily: 'Poppins',
+  color: Colors.white,
+  fontWeight: FontWeight.w300,
+  fontSize: 11,
+); 
+TextStyle cariStyle = TextStyle (
+  fontFamily: 'Poppins',
+  color: Colors.grey.withOpacity(0.8),
+  fontWeight: FontWeight.w400,
+  
+  fontSize: 12,
+); 

d52d5035e0a9ac0d17461fd6be46c261abc74084
Iniwizzy <badawilovina@gmail.com>
2024-10-23 23:11:19 +0700

    menambah theme

--- lib/config/theme.dart
+++ lib/config/theme.dart
@@ -3,8 +3,8 @@ import 'package:google_fonts/google_fonts.dart';
 
 Color whitecolor = const Color(0xFFFFFFFF);
 
-TextStyle titleTextStyle = GoogleFonts.poppins (
-  color : whitecolor,
+TextStyle titleTextStyle = GoogleFonts.poppins(
+  color: whitecolor,
   fontWeight: FontWeight.bold,
   fontSize: 26,
 );
@@ -15,7 +15,7 @@ TextStyle titleProTextStyle = GoogleFonts.poppins(
   fontSize: 26,
 );
 
-TextStyle subTitleTextStyle = GoogleFonts.poppins (
+TextStyle subTitleTextStyle = GoogleFonts.poppins(
   color: const Color(0xff899788),
   fontWeight: FontWeight.w300,
   fontSize: 16,
@@ -27,11 +27,11 @@ TextStyle plainTextStyle = GoogleFonts.poppins(
   fontSize: 18,
 );
 
-TextStyle decTextStyle = GoogleFonts.poppins (
+TextStyle decTextStyle = GoogleFonts.poppins(
   color: whitecolor,
   fontWeight: FontWeight.w300,
   fontSize: 12,
-); 
+);
 
 TextStyle priceTextStyle = GoogleFonts.poppins(
   color: whitecolor,
@@ -45,13 +45,13 @@ TextStyle buttonTextStyle = GoogleFonts.poppins(
   fontSize: 16,
 );
 
-TextStyle buttonStyle =  GoogleFonts.poppins(
+TextStyle buttonStyle = GoogleFonts.poppins(
   color: const Color(0xffffffff),
   fontWeight: FontWeight.w700,
   fontSize: 16,
 );
 
-TextStyle notifikasiStyle =  GoogleFonts.poppins(
+TextStyle notifikasiStyle = GoogleFonts.poppins(
   color: const Color(0xffffffff),
   fontWeight: FontWeight.w500,
   fontSize: 20,
@@ -63,13 +63,247 @@ TextStyle lampiranStyle = GoogleFonts.poppins(
   fontSize: 14,
 );
 
-TextStyle dec2TextStyle = GoogleFonts.poppins (
+TextStyle dec2TextStyle = GoogleFonts.poppins(
   color: Colors.black,
   fontWeight: FontWeight.w400,
   fontSize: 11,
-); 
-TextStyle dec3TextStyle = GoogleFonts.poppins (
+);
+TextStyle dec3TextStyle = GoogleFonts.poppins(
   color: Colors.white,
   fontWeight: FontWeight.w300,
   fontSize: 11,
-); 
+);
+// Color Palettes
+Color primary = const Color(0xFF12215C);
+Color secondary = const Color(0xFF489DD6);
+Color tertiary = const Color(0xFFA7ADC3);
+Color white = const Color(0xFFFFFFFF);
+
+// Font Styles
+TextStyle superFont1 = TextStyle(
+  fontFamily: 'Poppins',
+  fontWeight: FontWeight.w900,
+  fontSize: 19,
+  color: primary,
+);
+
+TextStyle superFont2 = TextStyle(
+  fontFamily: 'Poppins',
+  fontWeight: FontWeight.bold,
+  fontSize: 16,
+  color: primary,
+);
+
+TextStyle superFont3 = TextStyle(
+  fontFamily: 'Poppins',
+  fontWeight: FontWeight.bold,
+  fontSize: 14,
+  color: primary,
+);
+
+TextStyle superFont4 = TextStyle(
+  fontFamily: 'Poppins',
+  fontWeight: FontWeight.bold,
+  fontSize: 12,
+  color: primary,
+);
+
+TextStyle superFont5 = TextStyle(
+  fontFamily: 'Poppins',
+  fontWeight: FontWeight.bold,
+  fontSize: 10,
+  color: primary,
+);
+
+TextStyle superFont6 = TextStyle(
+  fontFamily: 'Poppins',
+  fontWeight: FontWeight.bold,
+  fontSize: 8,
+  color: primary,
+);
+
+// Regular Font Styles
+TextStyle regulerFont1 = TextStyle(
+  fontFamily: 'Source Sans Pro',
+  fontWeight: FontWeight.normal,
+  fontSize: 14,
+  color: primary,
+);
+
+TextStyle regulerFontSelected1 = TextStyle(
+  fontFamily: 'Source Sans Pro',
+  fontWeight: FontWeight.normal,
+  fontSize: 14,
+  color: white,
+);
+
+TextStyle regulerFont2 = TextStyle(
+  fontFamily: 'Source Sans Pro',
+  fontWeight: FontWeight.bold,
+  fontSize: 12,
+  color: primary,
+);
+
+TextStyle regulerFont3 = TextStyle(
+  fontFamily: 'Source Sans Pro',
+  fontWeight: FontWeight.normal,
+  fontSize: 10,
+  color: primary,
+);
+
+TextStyle regulerFont4 = TextStyle(
+  fontFamily: 'Source Sans Pro',
+  fontWeight: FontWeight.normal,
+  fontSize: 9,
+  color: primary,
+);
+
+TextStyle regulerFont5 = TextStyle(
+  fontFamily: 'Source Sans Pro',
+  fontWeight: FontWeight.normal,
+  fontSize: 5,
+  color: primary,
+);
+
+TextStyle regulerFont6 = TextStyle(
+  fontFamily: 'Source Sans Pro',
+  fontWeight: FontWeight.normal,
+  fontSize: 10,
+  color: primary,
+);
+
+TextStyle regulerFont7 = TextStyle(
+  fontFamily: 'Source Sans Pro',
+  fontWeight: FontWeight.normal,
+  fontSize: 10,
+  color: secondary,
+);
+
+TextStyle regulerFont8 = TextStyle(
+  fontFamily: 'Source Sans Pro',
+  fontWeight: FontWeight.normal,
+  fontSize: 4,
+  color: secondary,
+);
+
+TextStyle regulerFont9 = TextStyle(
+  fontFamily: 'Source Sans Pro',
+  fontWeight: FontWeight.normal,
+  fontSize: 12,
+  color: tertiary,
+);
+
+TextStyle regulerFont10 = TextStyle(
+  fontFamily: 'Source Sans Pro',
+  fontWeight: FontWeight.normal,
+  fontSize: 9,
+  color: tertiary,
+);
+
+// Button Font Styles
+TextStyle buttonFont1 = TextStyle(
+  fontFamily: 'Poppins',
+  fontWeight: FontWeight.bold,
+  fontSize: 15,
+  color: white,
+);
+
+TextStyle buttonFont2 = TextStyle(
+  fontFamily: 'Poppins',
+  fontWeight: FontWeight.bold,
+  fontSize: 14,
+  color: white,
+);
+
+TextStyle buttonFont3 = TextStyle(
+  fontFamily: 'Poppins',
+  fontWeight: FontWeight.bold,
+  fontSize: 13,
+  color: white,
+);
+
+TextStyle buttonFont4 = TextStyle(
+  fontFamily: 'Poppins',
+  fontWeight: FontWeight.bold,
+  fontSize: 12,
+  color: white,
+);
+
+TextStyle buttonFont5 = TextStyle(
+  fontFamily: 'Poppins',
+  fontWeight: FontWeight.bold,
+  fontSize: 10,
+  color: white,
+);
+
+TextStyle buttonFont6 = TextStyle(
+  fontFamily: 'Poppins',
+  fontWeight: FontWeight.bold,
+  fontSize: 8,
+  color: white,
+);
+
+TextStyle buttonFont7 = TextStyle(
+  fontFamily: 'Poppins',
+  fontWeight: FontWeight.bold,
+  fontSize: 7,
+  color: white,
+);
+
+// Button Styles //
+
+ButtonStyle longButton1 = ElevatedButton.styleFrom(
+  minimumSize: const Size(double.infinity, 37),
+  backgroundColor: secondary,
+  shape: RoundedRectangleBorder(
+    borderRadius: BorderRadius.circular(3),
+  ),
+);
+
+ButtonStyle longButton2 = ElevatedButton.styleFrom(
+  minimumSize: const Size(301, 37),
+  backgroundColor: const Color(0xFFA8E911),
+  shape: RoundedRectangleBorder(
+    borderRadius: BorderRadius.circular(5),
+  ),
+);
+
+ButtonStyle longButton3 = ElevatedButton.styleFrom(
+  minimumSize: const Size(301, 37),
+  backgroundColor: const Color(0xFFFF1F00),
+  shape: RoundedRectangleBorder(
+    borderRadius: BorderRadius.circular(5),
+  ),
+);
+
+ButtonStyle shortButton1 = ElevatedButton.styleFrom(
+    minimumSize: const Size(175, 32),
+    backgroundColor: secondary,
+    shape: RoundedRectangleBorder(
+      borderRadius: BorderRadius.circular(18),
+    ),
+    padding: const EdgeInsets.symmetric(vertical: 13, horizontal: 20));
+
+ButtonStyle shortButtonPrimary = ElevatedButton.styleFrom(
+    minimumSize: const Size(131, 32),
+    backgroundColor: primary,
+    shape: RoundedRectangleBorder(
+      borderRadius: BorderRadius.circular(18),
+    ),
+    padding: const EdgeInsets.symmetric(vertical: 13, horizontal: 20));
+
+ButtonStyle shortButtonSecondary = ElevatedButton.styleFrom(
+    minimumSize: const Size(131, 32),
+    backgroundColor: secondary,
+    shape: RoundedRectangleBorder(
+      borderRadius: BorderRadius.circular(18),
+    ),
+    padding: const EdgeInsets.symmetric(vertical: 13, horizontal: 20));
+
+ButtonStyle shortButton2 = ElevatedButton.styleFrom(
+    minimumSize: const Size(82, 22),
+    backgroundColor: secondary,
+    shape: RoundedRectangleBorder(
+      borderRadius: BorderRadius.circular(5),
+    ),
+    padding: const EdgeInsets.symmetric(vertical: 13, horizontal: 20));

cb9104fa7215611098f2cf8d252dc4cf57dfb469
Iniwizzy <badawilovina@gmail.com>
2024-10-23 23:11:41 +0700

    update main

--- lib/main.dart
+++ lib/main.dart
@@ -4,11 +4,14 @@ import 'package:arena_connect/authentication/login.dart';
 import 'package:arena_connect/authentication/register.dart';
 import 'package:arena_connect/homescreen.dart';
 import 'package:arena_connect/beranda/beranda_screen.dart';
-import 'package:arena_connect/kalender/kalender.dart';
+import 'package:arena_connect/kalender/kalender_screen.dart';
+import 'package:arena_connect/kalender/kalender_detail.dart';
 import 'package:arena_connect/tambah-lapangan/tambah_lapangan.dart';
 import 'package:arena_connect/daftar_lapangan/daftarlapang.dart';
 import 'package:arena_connect/notif/notifikasi.dart';
-import 'package:arena_connect/profile/profile.dart';
+// import 'package:arena_connect/profile/profile.dart';
+import 'package:arena_connect/profile/profil1.dart';
+import 'package:arena_connect/profile/editprofil.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -28,15 +31,19 @@ class MainApp extends StatelessWidget {
       theme: ThemeData(primaryColor: Colors.white),
       initialRoute: '/',
       routes: {
-        '/': (context) => const HomeScreen(),
+        '/': (context) => const HomeScreen(
+              title: 'Beranda',
+            ),
         '/login': (context) => const LoginPage(),
         '/register': (context) => const RegisterPage(),
         '/beranda': (context) => const BerandaScreen(),
-        '/kalender': (context) => const KalenderScreen(),
+        '/kalender': (context) => KalenderScreen(),
+        '/kalenderdetail': (context) => const KalenderDetailJadwalScreen(),
         '/tambahlapangan': (context) => const TambahLapanganScreen(),
         '/daftarlapang': (context) => const DaftarLapang(),
         '/notifikasi': (context) => const SampleContainer(),
-        '/profil': (context) => const ProfileScreen(),
+        '/profil': (context) => ProfileScreen(),
+        '/editprofil': (context) => EditProfileScreen(),
       },
     );
   }

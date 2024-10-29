473a1bd8b7625d112f2291d82126b286e95f8998
neurrochmat <neurrochmat6@gmail.com>
2024-10-30 01:13:49 +0700

    menambahkan pesanan

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
-// import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
 import 'package:arena_connect/laporan_keuangan/lap_hari.dart';
 import 'package:arena_connect/pembayaran/pembayaran.dart';
 import 'package:arena_connect/pemesanan/pemesanan.dart';
-import 'package:flutter/material.dart';
+import 'package:arena_connect/screens/homepage/home.dart';
 import 'package:arena_connect/authentication/login.dart';
 import 'package:arena_connect/authentication/register.dart';
 import 'package:arena_connect/homescreen.dart';
@@ -12,18 +13,22 @@ import 'package:arena_connect/kalender/kalender_detail.dart';
 import 'package:arena_connect/tambah-lapangan/tambah_lapangan.dart';
 import 'package:arena_connect/daftar_lapangan/daftarlapang.dart';
 import 'package:arena_connect/notif/notifikasi.dart';
-// import 'package:arena_connect/profile/profile.dart';
 import 'package:arena_connect/profile/profil1.dart';
 import 'package:arena_connect/profile/editprofil.dart';
-// import 'package:arena_connect/laporan_keuangan/daily.dart';
-import 'package:arena_connect/beranda/home.dart';
+import 'package:device_preview/device_preview.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
+  // Jika menggunakan Firebase, aktifkan ini setelah konfigurasi Firebase berhasil
   // await Firebase.initializeApp();
 
-  runApp(const MainApp());
+  runApp(
+    DevicePreview(
+      enabled: !kReleaseMode, // Aktifkan hanya pada debug mode
+      builder: (context) => const MainApp(),
+    ),
+  );
 }
 
 class MainApp extends StatelessWidget {
@@ -34,11 +39,12 @@ class MainApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(primaryColor: Colors.white),
+      locale: DevicePreview.locale(context), // Mendukung pratinjau bahasa
+      builder: DevicePreview.appBuilder, // Builder untuk pratinjau perangkat
+      useInheritedMediaQuery: true, // Diperlukan untuk DevicePreview
       initialRoute: '/',
       routes: {
-        '/': (context) => const HomeScreen(
-              title: 'Beranda',
-            ),
+        '/': (context) => const HomeScreen(title: 'Beranda'),
         '/login': (context) => const LoginPage(),
         '/register': (context) => const RegisterPage(),
         '/beranda': (context) => const BerandaScreen(),
@@ -51,7 +57,7 @@ class MainApp extends StatelessWidget {
         '/editprofil': (context) => EditProfileScreen(),
         '/laporankeuangan': (context) => const LaporanKeuanganScreen(),
         '/home': (context) => const HomePage(),
-        '/pesanan': (context) => const PesananScreen(),
+        '/pesanan': (context) => OrderListScreen(),
         '/pembayaran': (context) => PembayaranScreen(),
       },
     );
--- lib/pemesanan/pemesanan.dart
+++ lib/pemesanan/pemesanan.dart
@@ -1,10 +1,153 @@
 import 'package:flutter/material.dart';
 
-class PesananScreen extends StatelessWidget {
-  const PesananScreen({super.key});
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Order List',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: OrderListScreen(),
+    );
+  }
+}
+
+class OrderListScreen extends StatefulWidget {
+  @override
+  _OrderListScreenState createState() => _OrderListScreenState();
+}
+
+class _OrderListScreenState extends State<OrderListScreen> {
+  List<Order> orders = [
+    Order(
+      name: 'Azizah Salsa',
+      activity: 'Lapangan Badminton 1',
+      date: '19/09/2024',
+    ),
+    Order(
+      name: 'Budiman',
+      activity: 'Lapangan Futsal 1',
+      date: '19/09/2024',
+    ),
+    Order(
+      name: 'Anggun Sri',
+      activity: 'Lapangan Futsal 1',
+      date: '19/09/2024',
+    ),
+    Order(
+      name: 'Nurul hidayah',
+      activity: 'Lapangan Badminton 1',
+      date: '19/09/2024',
+    ),
+    Order(
+      name: 'Kevin Sanjaya',
+      activity: 'Lapangan Badminton 1',
+      date: '18/09/2024',
+    ),
+  ];
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Daftar Pesanan'),
+        backgroundColor: Color(0xFF12215C),
+        titleTextStyle: TextStyle(
+          color: Colors.white,
+          fontSize: 18.0,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: ListView.separated(
+          itemCount: orders.length,
+          separatorBuilder: (context, index) => SizedBox(height: 16.0),
+          itemBuilder: (context, index) {
+            Order order = orders[index];
+            return Container(
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(8.0),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey.withOpacity(0.5),
+                    spreadRadius: 2,
+                    blurRadius: 5,
+                    offset: Offset(0, 3),
+                  ),
+                ],
+              ),
+              child: ListTile(
+                leading: CircleAvatar(
+                  backgroundColor: Color(0xFF12215C),
+                  child: Text(
+                    order.name[0],
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 16.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                title: Text(
+                  order.name,
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Color(0xFF12215C),
+                    fontSize: 16.0,
+                  ),
+                ),
+                subtitle: Text(
+                  '${order.activity} - ${order.date}',
+                  style: TextStyle(
+                    color: Colors.grey[600],
+                    fontSize: 14.0,
+                  ),
+                ),
+                trailing: ElevatedButton(
+                  onPressed: () {
+                    // Handle "Lihat Detail" button press
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Color(0xFF12215C),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8.0),
+                    ),
+                    padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+                  ),
+                  child: Text(
+                    'Lihat Detail',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 14.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
+            );
+          },
+        ),
+      ),
+    );
   }
+}
+
+class Order {
+  final String name;
+  final String activity;
+  final String date;
+
+  Order({
+    required this.name,
+    required this.activity,
+    required this.date,
+  });
 }
